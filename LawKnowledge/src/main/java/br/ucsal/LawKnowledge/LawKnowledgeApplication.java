bdc44608bb18b276abd58250cb7ee68679a64a7c
Anne <feitozanne1@gmail.com>
2025-10-22 10:14:04 -0300

    primeiro commit

---
+++ LawKnowledge/src/main/java/br/ucsal/LawKnowledge/LawKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package br.ucsal.LawKnowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawKnowledgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawKnowledgeApplication.class, args);
+	}
+
+}
---
+++ LawKnowledge/src/test/java/br/ucsal/LawKnowledge/LawKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ucsal.LawKnowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawKnowledgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
